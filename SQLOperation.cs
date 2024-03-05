d79a3390ad7794b952f9e7b8a2c081e65d1fbfcd
Ken <146753820+kensindyliu@users.noreply.github.com>
2024-03-05 15:14:38 -0500

    sql operation class
    里面的代码只是一个例子

---
+++ SQLOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataService
+{
+    public class SQLOperation
+    {
+        public List<trip> GetTrips()
+        {
+            List<trip> trips = new List<trip>();
+            ItineraryEntities ie = new ItineraryEntities();
+            trips= ie.trip.ToList();
+            return trips;
+        }
+
+        public bool DeleteTripRepo(int tripId)
+        {
+            ItineraryEntities ie = new ItineraryEntities();
+            var trip = ie.trip.Where(x => x.TripID == tripId).FirstOrDefault();
+            if (trip != null)
+            {
+                ie.trip.Remove(trip);
+                ie.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
