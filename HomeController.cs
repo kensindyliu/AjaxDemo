b6c2b50bb0bca5d67e0e36660fecba1e27236d52
Ken <146753820+kensindyliu@users.noreply.github.com>
2024-03-05 15:11:36 -0500

    first time upload
    to show to to use ajax

---
+++ HomeController.cs
@@ -0,0 +1,61 @@
+﻿using AjaxDemo.Models;
+using DataService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public JsonResult GetTrips()
+        {
+            SQLOperation s = new SQLOperation();
+            //List<TripVM> trips = new List<TripVM>();
+            //var trlist = s.GetTrips();
+            //foreach (trip vm in trlist)
+            //{
+            //    trips.Add(new TripVM
+            //    {
+            //        TripID = vm.TripID,
+            //        Description = vm.Description,
+            //        Destination = vm.Destination,
+            //        StartDate = Convert.ToString(vm.StartDate),
+            //        EndDate = Convert.ToString(vm.EndDate)
+            //    });
+            //}
+
+            List<trip> trips = s.GetTrips();
+            return Json(trips, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteTrip(int TripID)
+        {
+            //TripService tr = new TripService();
+            SQLOperation s = new SQLOperation();
+            var result = s.DeleteTripRepo(TripID);
+            if (result)
+            {
+                return Json(JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+    }
+}
